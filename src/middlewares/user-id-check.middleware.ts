d2625950d483915deeb2080ea369516ced7268cb
Leonardo Rodrigues <leordoria2016@gmail.com>
2026-01-31 18:45:13 -0300

    (feat): Middleware para checagem de id no parâmetro da rota

---
+++ src/middlewares/user-id-check.middleware.ts
@@ -0,0 +1,13 @@
+import { BadGatewayException, NestMiddleware } from "@nestjs/common";
+import { NextFunction, Request, Response } from "express";
+
+export class UserIdCheckMiddleware implements NestMiddleware {
+    use(req: Request, res: Response, next: NextFunction) {
+        console.log('UserIdCheckMiddleware', 'antes')
+        if (isNaN(Number(req.params.id)) || Number(req.params.id) <= 0){
+            throw new BadGatewayException('ID inválido')
+        }
+        console.log('UserIdCheckMiddleware', 'antes')
+        next();
+    }    
+}
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { OtpModule } from 'src/otp/otp.module';
 import { EmailModule } from 'src/email/email.module';
+import { UserIdCheckMiddleware } from 'src/middlewares/user-id-check.middleware';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User]), OtpModule, EmailModule],
@@ -12,4 +13,11 @@ import { EmailModule } from 'src/email/email.module';
   providers: [UserService],
   exports: [UserService]
 })
-export class UserModule {}
+export class UserModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(UserIdCheckMiddleware).forRoutes({
+      path: 'user/:id',
+      method: RequestMethod.ALL
+    })
+  }
+}
